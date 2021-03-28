9d42addf61d6d90326cf09afb0a46a387f975a19
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2021-03-28 15:07:07 -0300

    feat: config socket in client

--- client/index.js
+++ client/index.js
@@ -1,9 +1,24 @@
+/**
+ node index.js \
+    --username matheusflauzino \
+    --room sala01 \
+    --hostUri localhost
+ */
+
 import Events from 'events'
+import CliConfig from './src/cliConfig.js';
+import SocketClient from './src/socket.js';
 import TerminalController from "./src/terminalController.js";
 
 
 const componentEmitter = new Events()
 const controller = new TerminalController()
 
-await controller.initializeTable(componentEmitter)
+const [nodePath, filePath, ...commands] = process.argv
+const config = CliConfig.parseArguments(commands)
+
+const socketClient = new SocketClient(config)
+
+await socketClient.initialize()
+// await controller.initializeTable(componentEmitter)
 
